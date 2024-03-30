e11d477ac6a282f4859d16c8620fca798c856c63
Łukasz Surdy <lukasz72221@gmail.com>
2024-03-30 14:11:05 +0100

    Release version 1.0.2

--- src/Dto/ImageSourceUrl.php
+++ src/Dto/ImageSourceUrl.php
@@ -6,12 +6,18 @@ namespace Lssoftware\ImageDownloader\Dto;
 
 use InvalidArgumentException;
 
+use function preg_match;
+
 readonly class ImageSourceUrl
 {
     public function __construct(public string $value)
     {
         if (empty($this->value)) {
             throw new InvalidArgumentException("image source cant be empty");
+        }
+
+        if (!preg_match("#^https?://.+#", $this->value)) {
+            throw new InvalidArgumentException('source is not valid url');
         }
     }
 }
